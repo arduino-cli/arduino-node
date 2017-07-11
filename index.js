a4c618bdb1cc075a514ef3f8600f1c924ec6b76a
simonepri <simone.pri@hotmail.it>
2017-07-11 23:11:12 +0200

    Revert the latest commit

--- index.js
+++ index.js
@@ -222,7 +222,7 @@ function arduino(opts) {
     if (semver().lt(version, '1.5.2')) {
       throw new Error('Arduino command line options are avaiable from the version 1.5.2');
     }
-    const slug = path.join('arduino-' + version + binSlug, 'arduino-' + version);
+    const slug = 'arduino-' + version + binSlug;
     bin = manager()(binPath, slug);
     MIRRORS.forEach(mirror => {
       bin.src(mirror.uri.replace('{{version}}', version), mirror.os, mirror.arch);
