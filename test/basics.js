dee7fa9f0bc0d977947c3273f7eef26c8e03dfc9
Emanuele Volanti <emavolant@gmail.com>
2017-07-27 15:20:43 +0200

    Add tests

--- test/basics.js
+++ test/basics.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import del from 'del';
 import pify from 'pify';
 import executable from 'executable';
@@ -5,13 +6,15 @@ import exists from 'exists-file';
 import test from 'ava';
 import arduino from '../index';
 
-test('arduino should fail to download versions not available', async t => {
+const fixture = path.join.bind(path, 'fixtures');
+
+test.serial('arduino should fail to download versions not available', async t => {
   const arduinoObj = arduino({version: '1.5.0'});
   const err = await t.throws(pify(arduinoObj.load)());
   t.is(err.message, 'The version provided is not available');
 });
 
-test('arduino should load and unload arduino latest', async t => {
+test.serial('arduino should load and unload arduino latest.serial', async t => {
   const arduinoObj = arduino({path: 'tmp', tag: 'load'});
   let err = await pify(arduinoObj.load)();
   t.is(err, undefined);
@@ -24,6 +27,21 @@ test('arduino should load and unload arduino latest', async t => {
   t.is(result, false);
 });
 
-test.after('cleanup', async t => {
+test.serial('arduino should not download the IDE twice', async t => {
+  const arduinoObj = arduino({path: 'tmp', tag: 'load'});
+  const err = await pify(arduinoObj.load)();
+  t.is(err, undefined);
+  const err2 = await pify(arduinoObj.load)();
+  t.is(err2, undefined);
+});
+
+test.serial('arduino should run succesfully if not loaded', async t => {
+  const arduinoObj = arduino({tag: 'run'});
+  const out = await pify(arduinoObj.run)(['--verify', fixture('empty/empty.ino')]);
+  t.is(out.failed, false);
+  arduinoObj.unload();
+});
+
+test.serial.after('cleanup', async t => {
   await t.notThrows(del('tmp'));
 });
